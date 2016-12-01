db1d4bd3a5abaf237b2aba3a138aa15bc334fafb
twelve <cntwelve@outlook.com>
2016-12-01 23:45:15 +0800

    add arg parse and dup lines check

--- merge_lines_for_fpg/merge.py
+++ merge_lines_for_fpg/merge.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 import datetime
+import sys
+import os
 
-file_obj = open('putty.log')
+file_name = ""
+if len(sys.argv) == 2:
+    file_name = sys.argv[1]
+else:
+    file_name = "putty.log"
+
+if not os.path.exists(file_name):
+    print("File is NOT exist!")
+    sys.exit(1)
+
+file_obj = open(file_name, encoding='utf-8')
 all_lines = file_obj.readlines()
 line_number = len(all_lines)
 for i in range(line_number):
@@ -16,7 +28,14 @@ for i in range(0, (line_number - 4)):
     for j in range(1, 5):
         # print("   --- j = %d" % (j))
         if (datetime.datetime.strptime(all_lines[i + j].split("\t")[0], "%Y-%m-%d %H:%M:%S") - datetime.datetime.strptime(all_lines[i].split("\t")[0], "%Y-%m-%d %H:%M:%S")).total_seconds() <= 300:
-            line_to_be_write += (" " + all_lines[i + j].split("\t")[1])
+            add = True
+            # 检查是否有重复的告警
+            for k in range(0, j):
+                if all_lines[(i + j)].split("\t")[1] == all_lines[(i + k)].split("\t")[1]:
+                    add = False
+                    break
+            if add:
+                line_to_be_write += (" " + all_lines[i + j].split("\t")[1])
         else:
             break
     print(line_to_be_write)
