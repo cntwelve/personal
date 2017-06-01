b6af5151ed373bba87fb18cf93f031dff5588a50
twelve <cntwelve@outlook.com>
2017-06-01 10:39:57 +0800

    kafka example

---
+++ HelloKafka/src/main/java/com/ctc/sd/HelloKafka.java
@@ -0,0 +1,29 @@
+package com.ctc.sd;
+
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+import java.util.Random;
+
+public class HelloKafka {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("zookeeper.connect", "192.168.25.100:2181,192.168.25.101:2181,192.168.25.102:2181");
+        properties.put("serializer.class", "kafka.serializer.StringEncoder");
+        properties.put("bootstrap.servers", "192.168.25.100:6667");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(properties);
+while (true) {
+    Random rand = new Random();
+    
+    producer.send(new ProducerRecord<String, String>("testn", "Message from test program."));
+    break;
+}
+        producer.close();
+    }
+}
---
+++ HelloKafka/src/main/java/com/ctc/sd/HelloKafkaConsumer.java
@@ -0,0 +1,44 @@
+package com.ctc.sd;
+
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class HelloKafkaConsumer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("zookeeper.connect", "192.168.25.100:2181,192.168.25.101:2181,192.168.25.102:2181");
+
+        props.put("bootstrap.servers", "192.168.25.100:6667");
+//        System.out.println("this is the group part test 1");
+//        消费者的组id
+        props.put("group.id", "GroupA");
+//        props.put("enable.auto.commit", "true");
+//        props.put("auto.commit.interval.ms", "1000");
+
+        //从poll(拉)的回话处理时长
+//        props.put("session.timeout.ms", "30000");
+        //poll的数量限制
+//props.put("max.poll.records", "100");
+
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        //订阅主题列表topic
+        consumer.subscribe(Arrays.asList("testn"));
+
+        ConsumerRecords<String, String> records = consumer.poll(100);
+        for (ConsumerRecord<String, String> record : records)
+            //　正常这里应该使用线程池处理，不应该在这里处理
+            System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value() + "\n");
+
+    }
+}
