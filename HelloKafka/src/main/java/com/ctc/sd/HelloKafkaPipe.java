dccc0b5419d9d896ca155deb49f503f4f9bb6256
twelve <cntwelve@outlook.com>
2017-06-01 21:48:30 +0800

    kafka streams

---
+++ HelloKafka/src/main/java/com/ctc/sd/HelloKafkaPipe.java
@@ -0,0 +1,38 @@
+package com.ctc.sd;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+
+import java.util.Properties;
+
+/**
+ * Created by lenovo on 2017/6/1.
+ */
+public class HelloKafkaPipe {
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.25.100:6667");
+        props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KStreamBuilder builder = new KStreamBuilder();
+
+        builder.stream("testn").to("testm");
+
+        KafkaStreams streams = new KafkaStreams(builder, props);
+        streams.start();
+
+        // usually the stream application would be running forever,
+        // in this example we just let it run for some time and stop since the input data is finite.
+        Thread.sleep(30000L);
+
+        streams.close();
+    }
+}
