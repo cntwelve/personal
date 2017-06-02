15739ed9ae099dbbbefdac875871f52f6b509dea
twelve <cntwelve@outlook.com>
2017-06-02 16:04:21 +0800

    wordcount

--- HelloKafka/src/main/java/com/ctc/sd/HelloKafkaStreamWordCount.java
+++ HelloKafka/src/main/java/com/ctc/sd/HelloKafkaStreamWordCount.java
@@ -1,6 +1,7 @@
 package com.ctc.sd;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
@@ -16,11 +17,17 @@ import java.util.Properties;
  */
 public class HelloKafkaStreamWordCount {
     public static void main(String[] args) throws Exception {
+        final Serde<String> stringSerde = Serdes.String();
+        final Serde<Long> longSerde = Serdes.Long();
+
+//        System.out.println(System.getProperty("java.library.path"));
+
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.25.100:6667");
         props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.STATE_DIR_CONFIG, "/tmp");
 
         // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
         // Note: To re-run the demo, you need to use the offset reset tool:
@@ -29,8 +36,9 @@ public class HelloKafkaStreamWordCount {
 
         KStreamBuilder builder = new KStreamBuilder();
 
-        KStream<String, String> source = builder.stream("streams-file-input");
+        KStream<String, String> source = builder.stream("testn");
 
+/*
         KTable<String, Long> counts = source
                 .flatMapValues(new ValueMapper<String, Iterable<String>>() {
                     @Override
@@ -47,14 +55,21 @@ public class HelloKafkaStreamWordCount {
                 .count("Counts");
 
         // need to override value serde to Long type
-        counts.to(Serdes.String(), Serdes.Long(), "streams-wordcount-output");
+        counts.to(Serdes.String(), Serdes.Long(), "testm");
+*/
+
+        source
+                .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+                .map((key, value) -> new KeyValue<>(value, value))
+                .countByKey(stringSerde, "Counts")
+                .to(stringSerde, longSerde, "testm");
 
         KafkaStreams streams = new KafkaStreams(builder, props);
         streams.start();
 
         // usually the stream application would be running forever,
         // in this example we just let it run for some time and stop since the input data is finite.
-        Thread.sleep(5000L);
+        Thread.sleep(30000L);
 
         streams.close();
     }
