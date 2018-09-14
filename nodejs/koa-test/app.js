3f70ad93b797a12b08509f8a5e76da0db611095c
twelve <cntwelve@outlook.com>
2018-09-14 15:38:22 +0800

    server and html
    server and html

--- nodejs/koa-test/app.js
+++ nodejs/koa-test/app.js
@@ -1,19 +1,21 @@
 const Koa = require('koa');
 const app = new Koa();
-// const { Pool } = require('pg')
+const { Pool } = require('pg')
 const cors = require('koa2-cors');
+const bodyParser = require('koa-bodyparser');
 
 app.use(cors());
+app.use(bodyParser());
 
-// const pool = new Pool({
-//     user: 'postgres',
-//     host: '127.0.0.1',
-//     database: 'postgres',
-//     password: 'postgres',
-//     port: 5432,
-// });
+const pool = new Pool({
+  user: 'z',
+  host: '127.0.0.1',
+  database: 'test',
+  password: 'z',
+  port: 5432,
+});
 
-// app.context.dbpool = pool;
+app.context.dbpool = pool;
 
 // app.use(async ctx => {
 //   ctx.body = 'Hello World';
@@ -29,13 +31,16 @@ app.use(cors());
 app.use(async ctx => {
   // ctx.set("Access-Control-Allow-Origin", "*");
   ctx.body = 'Hello World';
-  // const client = await ctx.dbpool.connect();
-  // try {
-  //   const res = await client.query('SELECT now() as now');
-  //   ctx.body += res.rows[0].now;
-  // } finally {
-  //   client.release();
-  // }
+  // console.log('ID: ' + ctx.query.id);
+  // console.log('温度: ' + ctx.query.temp);
+  // console.log('时间戳： ' + ctx.query.datetime);
+  const client = await ctx.dbpool.connect();
+  try {
+    const res = await client.query('insert into sensor(sensor_id, data, gen_time) values(' + ctx.query.id + ', ' + ctx.query.temp + ', to_timestamp(' + ctx.query.datetime + ' / 1000.0));');
+    // ctx.body += res.rows[0].now;
+  } finally {
+    client.release();
+  }
 });
 
 app.listen(8080);
